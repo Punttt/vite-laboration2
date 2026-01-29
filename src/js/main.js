2fb61cdb7759c6be01af212cc039925fb91c33f4
Pontus Johansson <johanssonp998@gmail.com>
2026-01-29 18:39:47 +0100

    börjat med fetch API i JS

--- src/js/main.js
+++ src/js/main.js
@@ -4,4 +4,20 @@ document.addEventListener("DOMContentLoaded", async ()=>{
 
 async function loadData(){
     const url = "https://webbutveckling.miun.se/files/ramschema.json";
+
+    try {
+        const response = await fetch(url);
+        const courses = await response.json();
+
+        displayCourses(courses);
+
+    } catch(error){
+        console.error("fel:", error);
+    }
+}
+
+function displayCourses(courses){
+    const courseTableEl = document.querySelector("#course-table-body");
+
+    console.table(courses);
 }
